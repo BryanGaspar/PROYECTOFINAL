822c1bb86d13d896bb5866492e6a6f78229325b2
BryanGaspar <bryangaspar202@gmail.com>
2022-02-18 16:17:12 -0500

    MENU

--- Proyecto/src/Diseño/MENU.java
+++ Proyecto/src/Diseño/MENU.java
@@ -6,6 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JDesktopPane;
+import javax.swing.JToolBar;
+import javax.swing.JMenuBar;
+import java.awt.Color;
+import javax.swing.JMenuItem;
+import javax.swing.SwingConstants;
+import javax.swing.JMenu;
 
 public class MENU extends JFrame {
 
@@ -31,12 +38,36 @@ public class MENU extends JFrame {
 	 * Create the frame.
 	 */
 	public MENU() {
+		setTitle("MENU DE OPCIONES");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 923, 568);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.WHITE);
+		menuBar.setBounds(0, 0, 907, 26);
+		contentPane.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("PROCESOS");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("ESTUDIANTES");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("MATERIAS");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu_1 = new JMenu("AYUDA");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("FUNCIONAMIENTO");
+		mnNewMenu_1.add(mntmNewMenuItem_2);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBounds(0, 25, 907, 504);
+		contentPane.add(desktopPane);
 	}
-
 }
